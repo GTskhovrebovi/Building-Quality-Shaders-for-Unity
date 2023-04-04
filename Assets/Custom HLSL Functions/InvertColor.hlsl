7457c61c7d4b87a9d113ff52ac5adf281965288c
Giorgi Tskhovrebovi <g.tskhovrebovi.91@gmail.com>
2023-04-05 02:02:21 +0400

    First shaders. Import some skyboxes.

---
+++ Assets/Custom HLSL Functions/InvertColor.hlsl
@@ -0,0 +1,8 @@
+﻿void InvertColor_float (float4 Color, out float4 Out)
+{
+    Out = 1.0f - Color;
+}
+void InvertColor_half (half4 Color, out half4 Out)
+{
+    Out = 1.0f - Color;
+}
---
+++ Assets/Scripts/MaterialShaderGizmoDrawer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MaterialShaderGizmoDrawer : MonoBehaviour
+{
+    private void OnDrawGizmosSelected()
+    {
+        var meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer == null) return;
+        
+        var materials = meshRenderer.sharedMaterials;
+        foreach (var material in materials)
+        {
+            if (material == null) continue;
+            
+            var tagTex = new Texture2D(1, 1);
+            tagTex.SetPixel(0, 0, new Color(0.4f, 0.4f, 0.4f, 0.8f)); // Set alpha to 0.8
+            tagTex.Apply();
+            
+            var shaderName = material.shader.name;
+            var style = new GUIStyle
+            {
+                normal =
+                {
+                    background = tagTex,
+                    textColor = new Color(0f, 0f, 0f, 1f) // Set alpha to 0.8
+                },
+                alignment = TextAnchor.MiddleCenter,
+                padding = new RectOffset(8, 8, 2, 2),
+                fontSize = 12,
+                fontStyle = FontStyle.Bold
+            };
+
+            var labelPos = transform.position + Vector3.up * (transform.localScale.y / 2 + 0.4f);
+            UnityEditor.Handles.Label(labelPos, shaderName, style);
+        }
+    }
+}
